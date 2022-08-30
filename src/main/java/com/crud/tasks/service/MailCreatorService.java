19102cc7a6540d3d165be10e66ede01837f8b180
Daniel Misior <daniel.misior09@gmail.com>
2022-08-30 17:00:11 +0200

    32.2. Task

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.trello.config.AdminConfig;
+import com.crud.tasks.trello.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,8 @@ public class MailCreatorService {
     @Autowired
     private AdminConfig adminConfig;
     @Autowired
+    private CompanyConfig companyConfig;
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
@@ -22,6 +25,11 @@ public class MailCreatorService {
         context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("regards", "Best regards,");
+        context.setVariable("your_app", "Your Application");
+        context.setVariable("companyName", companyConfig.getCompanyName());
+        context.setVariable("companyEmail", companyConfig.getCompanyEmail());
+        context.setVariable("companyPhone", companyConfig.getCompanyPhone());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
